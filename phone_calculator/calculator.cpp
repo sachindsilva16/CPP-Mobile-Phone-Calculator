43142ff000d337b0f299e5c7d26c49bb475ee8e1
sachindsilva16 <sachindsilvad@gmail.com>
2023-10-22 12:36:09 +0530

    Initial commit

---
+++ phone_calculator/calculator.cpp
@@ -0,0 +1,15 @@
+#include "calculator.h"
+#include "ui_calculator.h"
+
+calculator::calculator(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::calculator)
+{
+    ui->setupUi(this);
+}
+
+calculator::~calculator()
+{
+    delete ui;
+}
+
---
+++ phone_calculator/calculator.h
@@ -0,0 +1,21 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class calculator; }
+QT_END_NAMESPACE
+
+class calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    calculator(QWidget *parent = nullptr);
+    ~calculator();
+
+private:
+    Ui::calculator *ui;
+};
+#endif // CALCULATOR_H
